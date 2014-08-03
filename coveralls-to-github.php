591c69287b41d442d1584a11abebdb297bbeb18a
deedee <k4georges@gmail.com>
2014-08-03 02:29:15 +0200

    first

--- coveralls-to-github.php
+++ coveralls-to-github.php
@@ -1,18 +1,58 @@
 <?php
-echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
-$params = array('test' => 'Hello deedee !');
+$GITHUB_API_TOKEN = '%GITHUB_API_TOKEN%';
 
-$r = new HttpRequest('http://requestb.in/o18egeo1', HttpRequest::METH_POST);
-$r->setOptions(array('redirect' => 10));
-$r->setContentType ('application/json');
-$r->setBody(json_encode(params));
+$repo = $_POST['repo_name'];
+$commit = $_POST['commit_sha'];
+$branch = $_POST['branch'];
+$coverageChange = $_POST['coverage_change'];
 
-try {
-    $r->send();
-    if ($r->getResponseCode() == 200) {
-        //file_put_contents('local.rss', $r->getResponseBody());
+// Retrive previous comment
+$ch=curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/'.$repo.'/commits/'.$commit.'/comments');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: coveralls-to-github', 'Authorization: token '.$GITHUB_API_TOKEN));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$comments=curl_exec($ch);
+if(curl_errno($ch)){echo 'ERREUR GET curl_exec : '.curl_error($ch);}
+if(curl_getinfo($ch, CURLINFO_HTTP_CODE)!=200){echo 'HTTP GET code : '.curl_getinfo($ch, CURLINFO_HTTP_CODE).' received';die();}
+curl_close($ch);
+
+$comments = json_decode($comments);
+
+$commentId = -1;
+foreach($comments as $comment) {
+    if (strpos($comment->body, '[Coverage Status]') !== false) {
+        $commentId = $comment->id;
     }
-} catch (HttpException $ex) {
-    echo $ex;
+    echo $comment->body;
 }
+ 
+$message = '[![Coverage Status](https://coveralls.io/repos/'.$repo.'/badge.png?branch='.$branch.')](https://coveralls.io/r/'.$repo.'?branch='.$branch.') ';
+$covChange = floatval($coverageChange);
+if ($covChange > -0.01 && $covChange < 0.01)
+    $message .= '+0.0% :relieved:';
+else if ($covChange >= 0.01) 
+    $message .= '+'.$covChange.'% :kissing_heart:';
+else
+    $message .= '-'.$covChange.'% :scream:';
+
+//Coverage increased (+0.0%) when pulling a3264ae on eush77:bellman-ford-early-stop into a2d30b0 on felipernb:master.
+// Send comment
+$ch = curl_init();
+
+if ($commentId <= 0) {
+    curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/'.$repo.'/commits/'.$commit.'/comments' );
+    curl_setopt($ch, CURLOPT_POST, 1 );
+} else {
+    curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/'.$repo.'/comments/'.$commentId );
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH' );
+}
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('body' => $message)) ); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: coveralls-to-github', 'Content-Type: application/json', 'Authorization: token '.$GITHUB_API_TOKEN)); 
+
+$result=curl_exec($ch);
+if(curl_errno($ch)){echo 'ERREUR POST curl_exec : '.curl_error($ch);}
+if(curl_getinfo($ch, CURLINFO_HTTP_CODE)!=200 || curl_getinfo($ch, CURLINFO_HTTP_CODE)!=201){echo 'HTTP POST code : '.curl_getinfo($ch, CURLINFO_HTTP_CODE).' received';die();}
+curl_close($ch);
 ?>
